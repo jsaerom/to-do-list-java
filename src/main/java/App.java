b4d34b2c7fb4a44562db5af71c5343d82281afa1
Sheena Nickerson and Timothy Herold <timothy@>
2016-09-19 10:55:41 -0700

    Adjust front end to allow user to set/clear completed status and update description of a task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -25,26 +25,41 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/categories/:categoryId/tasks/:taskId", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Category category = Category.find(Integer.parseInt(request.params(":categoryId")));
+      Task task = Task.find(Integer.parseInt(request.params(":taskId")));
+      model.put("category", category);
+      model.put("task", task);
+      model.put("template", "templates/task-mark-complete.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/taskcomplete", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Task task = Task.find(Integer.parseInt(request.queryParams("taskId")));
+      if(request.queryParams("taskCompleted") != null) {
+        task.setCompleted(true);
+      } else {
+        task.setCompleted(false);
+      }
+      task.setDescription(request.queryParams("taskDescription"));
+      task.update();
+      model.put("template", "templates/task-complete-success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     post("/tasks", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       Category category = Category.find(Integer.parseInt(request.queryParams("categoryId")));
 
       String description = request.queryParams("description");
-      Task newTask = new Task(description);
-
-      category.addTask(newTask);
+      Task newTask = new Task(description, category.getId());
+      newTask.save();
 
       model.put("category", category);
       model.put("template", "templates/category-tasks-success.vtl");
       return new ModelAndView(model, layout);
-     }, new VelocityTemplateEngine());
-
-     post("/tasks", (request, response) -> {
-       Map<String, Object> model = new HashMap<String, Object>();
-       String description = request.queryParams("description");
-       Task newTask = new Task(description);
-       model.put("template", "templates/success.vtl");
-       return new ModelAndView(model, layout);
      }, new VelocityTemplateEngine());
 
      get("/tasks/:id", (request, response) -> {
@@ -65,6 +80,7 @@ public class App {
        Map<String, Object> model = new HashMap<String, Object>();
        String name = request.queryParams("name");
        Category newCategory = new Category(name);
+       newCategory.save();
        model.put("template" , "templates/category-success.vtl");
        return new ModelAndView(model, layout);
      }, new VelocityTemplateEngine());
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,60 +1,108 @@
 import java.time.LocalDateTime;
 import org.junit.*;
 import static org.junit.Assert.*;
+import org.sql2o.*;
 
 public class TaskTest {
 
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do_test", null, null);
+  }
+
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM tasks *;";
+      String deleteCategoriesQuery = "DELETE FROM categories *;";
+      con.createQuery(sql).executeUpdate();
+      con.createQuery(deleteCategoriesQuery).executeUpdate();
+    }
+  }
+
   @Test
-  public void Task_instantiatesCorrectly_true() {
-    Task myTask = new Task("Mow the lawn");
+  public void task_instantiatesCorrectly_true() {
+    Task myTask = new Task("Mow the lawn", 1);
     assertEquals(true, myTask instanceof Task);
   }
 
   @Test
-  public void Task_instantiatesWithDescription_String() {
-    Task myTask = new Task("Mow the lawn");
+  public void task_instantiatesWithDescription_String() {
+    Task myTask = new Task("Mow the lawn", 1);
+    myTask.save();
     assertEquals("Mow the lawn", myTask.getDescription());
   }
 
   @Test
   public void isCompleted_isFalseAfterInstantiation_false() {
-    Task myTask = new Task("Mow the lawn");
+    Task myTask = new Task("Mow the lawn", 1);
+    myTask.save();
     assertEquals(false, myTask.isCompleted());
   }
 
   @Test
   public void getCratedAt_instantiatesWithCurrentTime_today() {
-    Task myTask = new Task("Mow the lawn");
+    Task myTask = new Task("Mow the lawn", 1);
+    myTask.save();
     assertEquals(LocalDateTime.now().getDayOfWeek(), myTask.getCreatedAt().getDayOfWeek());
   }
 
   @Test
   public void all_returnsAllInstancesOfTask_true() {
-    Task firstTask = new Task("Mow the lawn");
-    Task secondTask = new Task("Buy groceries");
-    assertEquals(true, Task.all().contains(firstTask));
-    assertEquals(true, Task.all().contains(secondTask));
-  }
-
-  @Test
-  public void clear_emptiesAllTasksFromArrayList_0() {
-    Task myTask = new Task("Mow the lawn");
-    Task.clear();
-    assertEquals(Task.all().size(), 0);
+    Task firstTask = new Task("Mow the lawn", 1);
+    firstTask.save();
+    Task secondTask = new Task("Buy groceries", 1);
+    secondTask.save();
+    assertEquals(true, Task.all().get(0).equals(firstTask));
+    assertEquals(true, Task.all().get(1).equals(secondTask));
   }
 
   @Test
   public void getId_tasksInstanciateWithAnID_1() {
-    Task.clear();
-    Task myTask = new Task("Mow the lawn");
-    assertEquals(1, myTask.getId());
+    Task myTask = new Task("Mow the lawn", 1);
+    myTask.save();
+    assertTrue(myTask.getId() > 0);
   }
 
   @Test
   public void find_returnsTaskWithSameId_secondTask() {
-    Task firstTask = new Task("Mow the lawn");
-    Task secondTask = new Task("Buy groceries");
+    Task firstTask = new Task("Mow the lawn", 1);
+    firstTask.save();
+    Task secondTask = new Task("Buy groceries", 1);
+    secondTask.save();
     assertEquals(Task.find(secondTask.getId()), secondTask);
+  }
+
+  @Test
+  public void equals_returnsTrueIfDescriptionsAretheSame() {
+    Task firstTask = new Task("Mow the lawn", 1);
+    Task secondTask = new Task("Mow the lawn", 1);
+    assertTrue(firstTask.equals(secondTask));
+  }
+
+  @Test
+  public void save_returnsTrueIfDescriptionsAretheSame() {
+    Task myTask = new Task("Mow the lawn", 1);
+    myTask.save();
+    assertTrue(Task.all().get(0).equals(myTask));
+  }
+
+  @Test
+  public void save_assignsIdToObject() {
+    Task myTask = new Task("Mow the lawn", 1);
+    myTask.save();
+    Task savedTask = Task.all().get(0);
+    assertEquals(myTask.getId(), savedTask.getId());
+  }
+
+  @Test
+  public void save_savesCategoryIdIntoDB_true() {
+    Category myCategory = new Category("Household chores");
+    myCategory.save();
+    Task myTask = new Task("Mow the lawn", myCategory.getId());
+    myTask.save();
+    Task savedTask = Task.find(myTask.getId());
+    assertEquals(savedTask.getCategoryId(), myCategory.getId());
   }
 
 }
